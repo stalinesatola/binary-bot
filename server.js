fc9341b7e6c055a80d59277e08c402a31b94697e
PlumCodes <stalinesatola@gmail.com>
2022-08-11 09:38:05 +0100

    create server.js
    for app engine

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'www')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'www', 'index.html'));
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
