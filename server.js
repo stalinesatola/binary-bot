10088e2f2eb9ef98717336bb88d5dbd494cd85d8
PlumCodes <stalinesatola@gmail.com>
2022-08-11 12:05:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'www', 'index.html'));
 });
 
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
+// Listen to the App Engine-specified port, or 3000 otherwise
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
